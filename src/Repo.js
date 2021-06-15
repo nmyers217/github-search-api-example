3766ee57f078cf36731103e571bc04a4e6bf0af7
nmyers217 <ogremagi@DESKTOP-HORBCQQ.localdomain>
2021-06-14 20:50:23 -0500

    Add additional information to repositories

--- src/Repo.js
+++ src/Repo.js
@@ -2,18 +2,29 @@ function Repo({ repo = {} }) {
   return (
     <>
       <li key={repo.id}>
-        <a
-          className="App-link"
-          href={repo.html_url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {repo.full_name}
-        </a>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <span>👀{repo.watchers}</span>
+          </div>
+
+          <a
+            className="App-link"
+            href={repo.html_url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {repo.full_name}
+          </a>
+
+          <div>
+            <span>⭐{repo.stargazers_count}</span>
+          </div>
+        </div>
+
         <p>{repo.description}</p>
       </li>
 
-      <hr />
+      <hr style={{ borderBottom: '1rem' }} />
     </>
   );
 }
